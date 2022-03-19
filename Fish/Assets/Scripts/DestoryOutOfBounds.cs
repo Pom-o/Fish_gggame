51c43d83be53c42b2b6336016e81f8dcfa605e41
shidenggui <longlyshidenggui@gmail.com>
2022-03-19 21:20:30 +0800

    fix: left bound for destorying gameobject

--- Fish/Assets/Scripts/DestoryOutOfBounds.cs
+++ Fish/Assets/Scripts/DestoryOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestoryOutOfBounds : MonoBehaviour
 {
-    [SerializeField] float leftBound = -15;
+    [SerializeField] float leftBound = -6;
     [SerializeField] float bottomBound = -6f;
 
     // Start is called before the first frame update
