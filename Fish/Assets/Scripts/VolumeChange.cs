7a5e3e4c8ca267f79e44aa125dc53606ed48e9fe
iderin <iderin@outlook.com>
2022-03-20 22:30:24 +0000

    opt: change volume slider
    simplify code & change style for volume slider

--- Fish/Assets/Scripts/VolumeChange.cs
+++ Fish/Assets/Scripts/VolumeChange.cs
@@ -5,35 +5,35 @@ using UnityEngine.UI;
 
 public class VolumeChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     [SerializeField] Slider volumeSlider;
     void Start()
     {
-        if(!PlayerPrefs.HasKey("Volume")){
-            PlayerPrefs.SetFloat("Volume", 1);
-            Load();
-        }
+        // if(!PlayerPrefs.HasKey("Volume")){
+        //     PlayerPrefs.SetFloat("Volume", 1);
+        //     Load();
+        // }
 
-        else {
-            Load();
-        }
+        // else {
+        //     Load();
+        // }
+        volumeSlider.value = 2;
+        ChangeVolume();
     }
 
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
-        Save();
+        // Save();
 
     }
 
-    private void Load(){
-        volumeSlider.value = PlayerPrefs.GetFloat("Volume");
-    }
+    // private void Load(){
+    //     volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+    // }
 
-    private void Save(){
-        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
-    }
+    // private void Save(){
+    //     PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+    // }
 }
 
 
